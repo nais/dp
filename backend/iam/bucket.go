d4566d3b32cc016a9926121bb84cc1e4e7ad2fad
cteig <christine.teig@nav.no>
2021-05-08 20:57:50 +0200

    remove a member from a bucket

--- backend/iam/bucket.go
+++ backend/iam/bucket.go
@@ -2,6 +2,7 @@ package iam
 
 import (
 	"context"
+	"errors"
 	"google.golang.org/genproto/googleapis/type/expr"
 	"log"
 	"time"
@@ -65,6 +66,47 @@ func addUser(c *storage.Client, bucketName, member string) error {
 			Expression:  "request.time < timestamp('2022-12-31T12:00:00Z')",
 		},
 	})
+	if err := bucket.IAM().V3().SetPolicy(ctx, policy); err != nil {
+		return err
+	}
+	// NOTE: It may be necessary to retry this operation if IAM policies are
+	// being modified concurrently. SetPolicy will return an error if the policy
+	// was modified since it was retrieved.
+	return nil
+}
+
+func RemoveMemberFromBucket(bucketName, bucketMember string) error {
+	ctx := context.Background()
+	client, err := storage.NewClient(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ctx, cancel := context.WithTimeout(ctx, time.Second*10)
+	defer cancel()
+	bucket := client.Bucket(bucketName)
+	policy, err := bucket.IAM().V3().Policy(ctx)
+	if err != nil {
+		return err
+	}
+	for _, binding := range policy.Bindings {
+		// Only remove matching role
+		if binding.Role == "roles/storage.objectViewer" {
+			// Filter out member.
+			i := -1
+			for j, member := range binding.Members {
+				if member == bucketMember {
+					i = j
+				}
+			}
+
+			if i == -1 {
+				return errors.New("no matching binding group found")
+			} else {
+				binding.Members = append(binding.Members[:i], binding.Members[i+1:]...)
+			}
+		}
+	}
 	if err := bucket.IAM().V3().SetPolicy(ctx, policy); err != nil {
 		return err
 	}
--- backend/iam/bucket_test.go
+++ backend/iam/bucket_test.go
@@ -5,12 +5,23 @@ import (
 	"testing"
 )
 
-func TestBucketIam(t *testing.T) {
+func TestAddMemberToBucket(t *testing.T) {
 
 	bucketName := "container_resource_usage"
 	member := "user:christine.teig@nav.no"
 
 	err := iam.UpdateBucketAccessControl(bucketName, member)
+	if err != nil {
+		return
+	}
+}
+
+func TestRemoveMemberFromBucket(t *testing.T) {
+
+	bucketName := "container_resource_usage"
+	member := "user:christine.teig@nav.no"
+
+	err := iam.RemoveMemberFromBucket(bucketName, member)
 	if err != nil {
 		return
 	}
