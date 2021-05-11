4bf32a18ba1f43eb7135fa8dd343f1346397835a
Tore Sinding Bekkedal <tore.sinding.bekkedal@nav.no>
2021-05-11 11:00:41 +0200

    Remove redundant files which are redundant and unneccessarily duplicated to less redundant files

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,13 +1,37 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.less";
-import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { Hovedside } from "./hovedside";
+import { BrowserRouter as Router, Switch, Link, Route } from "react-router-dom";
+import ProduktDetalj from "./produktDetalj";
+import ProduktNytt from "./produktNytt";
+import { Sidetittel, Systemtittel } from "nav-frontend-typografi";
 
 ReactDOM.render(
   <React.StrictMode>
     <div className={"dashboard-main"}>
-      <App />
+      <div className="app">
+        <Router>
+          <header>
+            <Link to="/">
+              <object data="/nav-logo.svg" style={{ height: "5.625rem" }} />
+              <Systemtittel>Dataprodukter</Systemtittel>
+            </Link>
+          </header>
+
+          <main>
+            <Switch>
+              <Route path="/produkt/nytt" children={<ProduktNytt />} />
+              <Route path="/produkt/:produktID" children={<ProduktDetalj />} />
+              <Route exact path="/" children={<Hovedside />} />
+              <Route path="*">
+                <Systemtittel>404 - ikke funnet</Systemtittel>
+              </Route>
+            </Switch>
+          </main>
+        </Router>
+      </div>
     </div>
   </React.StrictMode>,
   document.getElementById("root")
