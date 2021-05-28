62ad9321b971c1d95ea0c16939f5b87775ef6716
Kent Daleng <kent.daleng@nav.no>
2021-05-28 12:58:00 +0200

    test some other firestore cases

--- backend/firestore/firestore_test.go
+++ backend/firestore/firestore_test.go
@@ -2,10 +2,11 @@ package firestore_test
 
 import (
 	"context"
-	"github.com/nais/dp/backend/firestore"
-	"github.com/stretchr/testify/assert"
 	"os"
 	"testing"
+
+	"github.com/nais/dp/backend/firestore"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestFirestoreWithEmulator(t *testing.T) {
@@ -30,4 +31,52 @@ func TestFirestoreWithEmulator(t *testing.T) {
 
 	dataproduct, err := f.GetDataproduct(context.Background(), dpID)
 	assert.Equal(t, testDataproduct.Name, dataproduct.Dataproduct.Name)
+}
+
+func TestUpdateDataproduct(t *testing.T) {
+	if os.Getenv("FIRESTORE_EMULATOR_HOST") == "" {
+		t.Skip("Skipping integration test")
+	}
+
+	f, err := firestore.New(context.Background(), "aura-dev-d9f5", "dp", "au")
+	assert.NoError(t, err)
+
+	dps, err := f.GetDataproducts(context.Background())
+	assert.NoError(t, err)
+	assert.Equal(t, 1, len(dps))
+
+	dp := dps[0]
+
+	assert.Equal(t, "dp", dp.Dataproduct.Name)
+	err = f.UpdateDataproduct(context.Background(), dp.ID, firestore.Dataproduct{Name: "dpdp"})
+	assert.NoError(t, err)
+
+	newDp, err := f.GetDataproduct(context.Background(), dp.ID)
+	assert.NoError(t, err)
+
+	assert.True(t, dp.Updated.Before(newDp.Updated))
+	assert.True(t, newDp.Created.Before(newDp.Updated))
+	assert.Equal(t, "dpdp", newDp.Dataproduct.Name)
+}
+
+func TestDeleteDataproduct(t *testing.T) {
+	if os.Getenv("FIRESTORE_EMULATOR_HOST") == "" {
+		t.Skip("Skipping integration test")
+	}
+
+	f, err := firestore.New(context.Background(), "aura-dev-d9f5", "dp", "au")
+	assert.NoError(t, err)
+
+	dps, err := f.GetDataproducts(context.Background())
+	assert.NoError(t, err)
+	assert.Equal(t, 1, len(dps))
+
+	dp := dps[0]
+
+	err = f.DeleteDataproduct(context.Background(), dp.ID)
+	assert.NoError(t, err)
+
+	dps, err = f.GetDataproducts(context.Background())
+	assert.NoError(t, err)
+	assert.Equal(t, 0, len(dps))
 }
