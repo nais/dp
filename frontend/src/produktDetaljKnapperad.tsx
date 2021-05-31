cc57da80132715cce606991b1b7a5bc9dacc24d2
frodesundby <frode.sundby@nav.no>
2021-05-31 10:52:15 +0200

    moving a bunch of functions instead of renaming

--- frontend/src/produktDetaljKnapperad.tsx
+++ frontend/src/produktDetaljKnapperad.tsx
@@ -1,48 +1,53 @@
-import { Fareknapp, Knapp } from "nav-frontend-knapper";
-import React, { useContext, useState } from "react";
-import { UserContext } from "./userContext";
+import {Fareknapp, Knapp} from "nav-frontend-knapper";
+import React, {useContext, useState} from "react";
+import {UserContext} from "./userContext";
 import {
-  DataProduktResponse,
-  DataProduktTilgangListe,
-  getCurrentAccessState,
-  hentProdukt,
-  isOwner,
+    DataProduktResponse,
+    DataProduktTilgangListe,
+    getCurrentAccessState,
+    hentProdukt,
+    isOwner,
 } from "./produktAPI";
 
 export const ProduktKnapperad: React.FC<{
-  produkt: DataProduktResponse;
-  tilganger: DataProduktTilgangListe;
-  openSlett: () => void;
-  openTilgang: () => void;
-}> = ({ produkt, tilganger, openSlett, openTilgang }) => {
-  const userContext = useContext(UserContext);
+    produkt: DataProduktResponse;
+    tilganger: DataProduktTilgangListe;
+    openSlett: () => void;
+    openTilgang: () => void;
+}> = ({produkt, tilganger, openSlett, openTilgang}) => {
+    const userContext = useContext(UserContext);
 
-  const harTilgang = (tilganger: DataProduktTilgangListe): boolean => {
-    if (!tilganger) return false;
-    const tilgangerBehandlet = getCurrentAccessState(tilganger);
-    if (!tilgangerBehandlet) return false;
+    const harTilgang = (tilganger: DataProduktTilgangListe): boolean => {
+        if (!tilganger) return false;
+        const tilgangerBehandlet = getCurrentAccessState(tilganger);
+        if (!tilgangerBehandlet) return false;
 
-    for (const tilgang of tilgangerBehandlet) {
-      if (tilgang.subject == userContext?.email) {
-        if (tilgang?.expires && new Date(tilgang.expires) > new Date()) {
-          return true;
+        for (const tilgang of tilgangerBehandlet) {
+            if (tilgang.subject == userContext?.email) {
+                if (tilgang?.expires && new Date(tilgang.expires) > new Date()) {
+                    return true;
+                }
+            }
         }
-      }
-    }
 
-    return false;
-  };
+        return false;
+    };
 
-  const ownsProduct = () => isOwner(produkt?.data_product, userContext?.teams)
-  return (
-    <div className="knapperad">
-      {ownsProduct() && (
-        <Fareknapp onClick={() => openSlett()}>Slett</Fareknapp>
-      )}
+    const ownsProduct = () => isOwner(produkt?.data_product, userContext?.teams)
+    return (
+        <div className="knapperad">
+            {ownsProduct() && (
+                <>
+                    <Fareknapp onClick={() => openSlett()}>Slett</Fareknapp>
+                    <Knapp onClick={() => {
+                    }}>Endre produkt</Knapp>
+                </>
+            )}
 
-      {userContext && !harTilgang(tilganger) && produkt?.data_product.datastore &&(
-        <Knapp onClick={() => openTilgang()}>Få tilgang</Knapp>
-      )}
-    </div>
-  );
-};
+            {userContext && !harTilgang(tilganger) && produkt?.data_product.datastore && (
+                <Knapp onClick={() => openTilgang()}>Få tilgang</Knapp>
+            )}
+        </div>
+    );
+}
+;
--- frontend/src/produktNytt.tsx
+++ frontend/src/produktNytt.tsx
@@ -92,111 +92,118 @@ const RessursVelger: React.FC<{
     </>
   );
 };
+
+const ProduktSkjema: React.FC<{
+    produktID: string | null;
+}> = ({ produktID}) => {
+    const history = useHistory();
+    const user = useContext(UserContext);
+    const [navn, setNavn] = useState<string>("");
+    const [beskrivelse, setBeskrivelse] = useState<string>("");
+    const [eier, setEier] = useState<string>("");
+    const [datastore, setDatastore] = useState<DataLager | null>(null);
+    const [formErrors, setFormErrors] = useState<ZodError["formErrors"]>();
+    const [dsFormErrors, setDsFormErrors] = useState<ZodError["formErrors"]>();
+    // Correctly initialize form to default value on page render
+    useEffect(() => setEier((e) => user?.teams?.[0] || e), [user]);
+
+    const parseDatastore = () => {
+        if (datastore?.type === "bigquery") {
+            DataLagerBigquerySchema.parse(datastore);
+        } else if (datastore?.type === "bucket") {
+            DataLagerBucketSchema.parse(datastore);
+        } else {
+            setDsFormErrors({
+                formErrors: [],
+                fieldErrors: { type: ["Required"] },
+            });
+        }
+    };
+
+    const handleSubmit = async (): Promise<void> => {
+        // First make sure we have a valid datastore
+        try {
+            parseDatastore();
+        } catch (e) {
+            setDsFormErrors(e.flatten());
+        }
+        try {
+            const nyttProdukt = DataProduktSchema.parse({
+                name: navn,
+                description: beskrivelse,
+                datastore: [datastore],
+                team: eier,
+                access: {},
+            });
+            const newID = await opprettProdukt(nyttProdukt);
+            history.push(`/produkt/${newID}`);
+        } catch (e) {
+            console.log(e.toString());
+
+            if (e instanceof ZodError) {
+                setFormErrors(e.flatten());
+            } else {
+                setFormErrors({ formErrors: e.toString(), fieldErrors: {} });
+            }
+        }
+    };
+    return (
+        <div style={{ margin: "1em 1em 0 1em" }}>
+            <SkjemaGruppe>
+                <Input
+                    label="Navn"
+                    feil={formErrors?.fieldErrors?.name}
+                    onChange={(e) => setNavn(e.target.value)}
+                />
+                <Input
+                    label="Beskrivelse"
+                    feil={formErrors?.fieldErrors?.description}
+                    onChange={(e) => setBeskrivelse(e.target.value)}
+                />
+                <Select
+                    label="Eier (team)"
+                    feil={formErrors?.fieldErrors?.owner}
+                    onChange={(e) => setEier(e.target.value)}
+                    children={
+                        user?.teams
+                            ? user.teams.map((t) => (
+                                <option key={t} value={t}>
+                                    {t}
+                                </option>
+                            ))
+                            : null
+                    }
+                />
+
+                <RessursVelger
+                    datastore={datastore}
+                    setDatastore={setDatastore}
+                    parseErrors={dsFormErrors}
+                />
+            </SkjemaGruppe>
+            {!!formErrors?.formErrors?.length && (
+                <Feilmelding>
+                    Feil: <br />
+                    <code>{formErrors.formErrors}</code>
+                </Feilmelding>
+            )}
+            <Hovedknapp
+                style={{ display: "block", marginLeft: "auto" }}
+                onClick={async () => {
+                    await handleSubmit();
+                }}
+            >
+                Submit
+            </Hovedknapp>
+        </div>
+    );
+}
+
 export const ProduktNytt = (): JSX.Element => {
-  const user = useContext(UserContext);
-  const history = useHistory();
-
-  const [navn, setNavn] = useState<string>("");
-  const [beskrivelse, setBeskrivelse] = useState<string>("");
-  const [eier, setEier] = useState<string>("");
-  // Correctly initialize form to default value on page render
-  useEffect(() => setEier((e) => user?.teams?.[0] || e), [user]);
-
-  const [datastore, setDatastore] = useState<DataLager | null>(null);
-
-  const [formErrors, setFormErrors] = useState<ZodError["formErrors"]>();
-  const [dsFormErrors, setDsFormErrors] = useState<ZodError["formErrors"]>();
-
-  const parseDatastore = () => {
-    if (datastore?.type === "bigquery") {
-      DataLagerBigquerySchema.parse(datastore);
-    } else if (datastore?.type === "bucket") {
-      DataLagerBucketSchema.parse(datastore);
-    } else {
-      setDsFormErrors({
-        formErrors: [],
-        fieldErrors: { type: ["Required"] },
-      });
-    }
-  };
-  const handleSubmit = async (): Promise<void> => {
-    // First make sure we have a valid datastore
-    try {
-      parseDatastore();
-    } catch (e) {
-      setDsFormErrors(e.flatten());
-    }
-    try {
-      const nyttProdukt = DataProduktSchema.parse({
-        name: navn,
-        description: beskrivelse,
-        datastore: [datastore],
-        team: eier,
-        access: {},
-      });
-      const newID = await opprettProdukt(nyttProdukt);
-      history.push(`/produkt/${newID}`);
-    } catch (e) {
-      console.log(e.toString());
-
-      if (e instanceof ZodError) {
-        setFormErrors(e.flatten());
-      } else {
-        setFormErrors({ formErrors: e.toString(), fieldErrors: {} });
-      }
-    }
-  };
-
-  return (
-    <div style={{ margin: "1em 1em 0 1em" }}>
-      <SkjemaGruppe>
-        <Input
-          label="Navn"
-          feil={formErrors?.fieldErrors?.name}
-          onChange={(e) => setNavn(e.target.value)}
-        />
-        <Input
-          label="Beskrivelse"
-          feil={formErrors?.fieldErrors?.description}
-          onChange={(e) => setBeskrivelse(e.target.value)}
-        />
-        <Select
-          label="Eier (team)"
-          feil={formErrors?.fieldErrors?.owner}
-          onChange={(e) => setEier(e.target.value)}
-          children={
-            user?.teams
-              ? user.teams.map((t) => (
-                  <option key={t} value={t}>
-                    {t}
-                  </option>
-                ))
-              : null
-          }
-        />
-
-        <RessursVelger
-          datastore={datastore}
-          setDatastore={setDatastore}
-          parseErrors={dsFormErrors}
-        />
-      </SkjemaGruppe>
-      {!!formErrors?.formErrors?.length && (
-        <Feilmelding>
-          Feil: <br />
-          <code>{formErrors.formErrors}</code>
-        </Feilmelding>
-      )}
-      <Hovedknapp
-        style={{ display: "block", marginLeft: "auto" }}
-        onClick={async () => {
-          await handleSubmit();
-        }}
-      >
-        Submit
-      </Hovedknapp>
-    </div>
-  );
+  return <ProduktSkjema produktID={""}></ProduktSkjema>
+};
+export const ProduktOppdatering = (): JSX.Element => {
+    return <ProduktSkjema produktID={""}></ProduktSkjema>
 };
 
 export default ProduktNytt;
