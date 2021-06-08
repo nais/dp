9af124930fc37a0dff477e1d429ed9af23ed812d
Johnny Horvi <johnny@horvi.no>
2021-06-08 07:43:43 +0200

    group tests with dependencies

--- backend/firestore/firestore_test.go
+++ backend/firestore/firestore_test.go
@@ -9,7 +9,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestFirestoreWithEmulator(t *testing.T) {
+func TestFirestoreCRUD(t *testing.T) {
 	if os.Getenv("FIRESTORE_EMULATOR_HOST") == "" {
 		t.Skip("Skipping integration test")
 	}
@@ -17,66 +17,54 @@ func TestFirestoreWithEmulator(t *testing.T) {
 	f, err := firestore.New(context.Background(), "aura-dev-d9f5", "dp", "au")
 	assert.NoError(t, err)
 
-	testDataproduct := firestore.Dataproduct{
-		Name:        "dp",
-		Description: "desc",
-		Datastore:   []map[string]string{{}},
-		Team:        "team",
-		Access:      nil,
-	}
+	t.Run("Create", func(t *testing.T) {
+		testDataproduct := firestore.Dataproduct{
+			Name:        "dp",
+			Description: "desc",
+			Datastore:   []map[string]string{{}},
+			Team:        "team",
+			Access:      nil,
+		}
 
-	dpID, err := f.CreateDataproduct(context.Background(), testDataproduct)
+		dpID, err := f.CreateDataproduct(context.Background(), testDataproduct)
 
-	assert.NoError(t, err)
+		assert.NoError(t, err)
 
-	dataproduct, err := f.GetDataproduct(context.Background(), dpID)
-	assert.Equal(t, testDataproduct.Name, dataproduct.Dataproduct.Name)
-}
+		dataproduct, err := f.GetDataproduct(context.Background(), dpID)
+		assert.Equal(t, testDataproduct.Name, dataproduct.Dataproduct.Name)
+	})
 
-func TestUpdateDataproduct(t *testing.T) {
-	if os.Getenv("FIRESTORE_EMULATOR_HOST") == "" {
-		t.Skip("Skipping integration test")
-	}
+	t.Run("Update", func(t *testing.T) {
+		dps, err := f.GetDataproducts(context.Background())
+		assert.NoError(t, err)
+		assert.Equal(t, 1, len(dps))
 
-	f, err := firestore.New(context.Background(), "aura-dev-d9f5", "dp", "au")
-	assert.NoError(t, err)
+		dp := dps[0]
 
-	dps, err := f.GetDataproducts(context.Background())
-	assert.NoError(t, err)
-	assert.Equal(t, 1, len(dps))
+		assert.Equal(t, "dp", dp.Dataproduct.Name)
+		err = f.UpdateDataproduct(context.Background(), dp.ID, firestore.Dataproduct{Name: "dpdp"})
+		assert.NoError(t, err)
 
-	dp := dps[0]
+		newDp, err := f.GetDataproduct(context.Background(), dp.ID)
+		assert.NoError(t, err)
 
-	assert.Equal(t, "dp", dp.Dataproduct.Name)
-	err = f.UpdateDataproduct(context.Background(), dp.ID, firestore.Dataproduct{Name: "dpdp"})
-	assert.NoError(t, err)
+		assert.True(t, dp.Updated.Before(newDp.Updated))
+		assert.True(t, newDp.Created.Before(newDp.Updated))
+		assert.Equal(t, "dpdp", newDp.Dataproduct.Name)
+	})
 
-	newDp, err := f.GetDataproduct(context.Background(), dp.ID)
-	assert.NoError(t, err)
+	t.Run("Delete", func(t *testing.T) {
+		dps, err := f.GetDataproducts(context.Background())
+		assert.NoError(t, err)
+		assert.Equal(t, 1, len(dps))
 
-	assert.True(t, dp.Updated.Before(newDp.Updated))
-	assert.True(t, newDp.Created.Before(newDp.Updated))
-	assert.Equal(t, "dpdp", newDp.Dataproduct.Name)
-}
+		dp := dps[0]
 
-func TestDeleteDataproduct(t *testing.T) {
-	if os.Getenv("FIRESTORE_EMULATOR_HOST") == "" {
-		t.Skip("Skipping integration test")
-	}
+		err = f.DeleteDataproduct(context.Background(), dp.ID)
+		assert.NoError(t, err)
 
-	f, err := firestore.New(context.Background(), "aura-dev-d9f5", "dp", "au")
-	assert.NoError(t, err)
-
-	dps, err := f.GetDataproducts(context.Background())
-	assert.NoError(t, err)
-	assert.Equal(t, 1, len(dps))
-
-	dp := dps[0]
-
-	err = f.DeleteDataproduct(context.Background(), dp.ID)
-	assert.NoError(t, err)
-
-	dps, err = f.GetDataproducts(context.Background())
-	assert.NoError(t, err)
-	assert.Equal(t, 0, len(dps))
+		dps, err = f.GetDataproducts(context.Background())
+		assert.NoError(t, err)
+		assert.Equal(t, 0, len(dps))
+	})
 }
